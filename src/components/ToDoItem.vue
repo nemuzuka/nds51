6cd5db75a40cf5b0b97820fd41583c11238c9c3c
Kazumune Katagiri <kkaz123@gmail.com>
2017-03-09 10:14:00 +0900

    一覧データ部のタグ変更

--- src/components/ToDoItem.vue
+++ src/components/ToDoItem.vue
@@ -1,9 +1,13 @@
 <template>
   <tr @click.stop="openModalDialog">
-    <th>{{index + 1}}</th>
-    <th>{{todo.context}}</th>
-    <th>{{todo.lastUpdateDate}}</th>
-    <th><button type="button" class="btn btn-danger btn-xs" @click.stop="deleteItem"><i class="glyphicon glyphicon-remove"></i></button></th>
+    <td>{{index + 1}}</td>
+    <td class="text-left">{{todo.context}}</td>
+    <td>{{todo.lastUpdateDate}}</td>
+    <td>
+      <button type="button" class="btn btn-danger btn-xs" @click.stop="deleteItem">
+        <i class="glyphicon glyphicon-remove"></i>
+      </button>
+    </td>
   </tr>
 </template>
 
